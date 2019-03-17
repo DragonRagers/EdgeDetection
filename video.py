c714e1fa72172b1dfb273000499263bd98f2119c
DragonRagers <dragonragers@gmail.com>
2019-03-17 18:04:56 -0400

    apply_async > apply by a lot

--- video.py
+++ video.py
@@ -4,7 +4,7 @@ from PIL import Image
 import moviepy.editor as mpy
 from multiprocessing import Pool
 
-input = mpy.VideoFileClip("test.mp4")
+input = mpy.VideoFileClip("videos\\test.mp4")
 
 #resize width to input maintaining aspect ratio
 def resize(img, basewidth = 500):
@@ -15,7 +15,7 @@ def resize(img, basewidth = 500):
 #runs frame at time t through edge detection filter
 ## TODO: change edge detection to run off of numpy instead of Pillow
 def edgeFrame(t):
-    return np.array(edge.edgeDetection(resize(Image.fromarray(input.get_frame(t)), 600),1,1))
+    return np.array(edge.edgeDetection(resize(Image.fromarray(input.get_frame(t)), 1000),1,1))
 
 
 #single thread approach
@@ -27,9 +27,10 @@ clip.write_gif("test.gif", fps = 1)
 #multiprocess edge detection processing per frame and stich them back together into a gif
 if __name__ == '__main__':
     time = 5
-    fps = 30
+    fps = 24
 
-    p = Pool(10)
-    imgs = [p.apply(edgeFrame, args=(t,)) for t in np.linspace(0,time,time*fps)]
+    p = Pool(8)
+    results = [p.apply_async(edgeFrame, args=(t,)) for t in np.linspace(0,time,time*fps)]
+    imgs = [p.get() for p in results]
     clip = mpy.ImageSequenceClip(imgs, fps)
     clip.write_gif("pew.gif")
