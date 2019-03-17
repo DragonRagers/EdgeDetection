ff782cf0dc6e02c272e0c082866ba5fea9ed4190
DragonRagers <dragonragers@gmail.com>
2019-03-16 22:34:08 -0400

    more multiprocessing things, framess currently output out of order

--- video.py
+++ video.py
@@ -2,7 +2,7 @@ import edge
 import numpy as np
 from PIL import Image
 import moviepy.editor as mpy
-from multiprocessing import Process, Lock
+from multiprocessing import Process, Manager
 
 input = mpy.VideoFileClip("test.mp4")
 
@@ -12,26 +12,35 @@ def resize(img, basewidth = 500):
     hsize = int((float(img.size[1])*float(wpercent)))
     return img.resize((basewidth,hsize))
 
-def edgeFrame(t):
-    #l.acquire()
-    img = edge.edgeDetection(resize(Image.fromarray(input.get_frame(t)), 1000),1,2)
-    return np.array(img)
-    #l.release()
+def edgeFrame(lock, t, list):
+    lock.acquire()
+    #img = edge.edgeDetection(resize(Image.fromarray(input.get_frame(t)), 600),1,2)
+    #list.append(np.array(img))
+    list.append(input.get_frame(t))
+    lock.release()
 
+#single thread approach
+"""
 clip = mpy.VideoClip(edgeFrame, duration=input.duration)
-clip.write_gif("test.gif", fps = 5)
-
+clip.write_gif("test.gif", fps = 1)
 """
+
+## TODO: while this does multiprocess, frames end up out of order
 if __name__ == '__main__':
-    lock = Lock()
-
+    numProcesses = 24
+    manager = Manager()
+    lock = manager.Lock()
+    imgList = manager.list()
     pool = []
-    for n in range(5):
-        pool.append(Process(target=edgeFrame, args=(lock, n)))
-    for i in range(5):
-        pool[i].start()
+
+    for n in range(numProcesses):
+        pool.append(Process(target=edgeFrame, args=(lock, n/6, imgList)))
+        pool[n].start()
     print("Start")
-    for i in range(5):
+    for i in range(numProcesses):
         pool[i].join()
-    print("Done: ")
-"""
+    print("Done")
+
+    normalList = [img for img in imgList]
+    clip = mpy.ImageSequenceClip(normalList, 3)
+    clip.write_gif("test.gif")
