3990781dc64bf3d7b08a281050ce2124d6830c3f
DragonRagers <dragonragers@gmail.com>
2019-03-16 23:50:55 -0400

    cleaning stuff up

--- video.py
+++ video.py
@@ -4,14 +4,16 @@ from PIL import Image
 import moviepy.editor as mpy
 from multiprocessing import Process, Manager, Pool
 
-input = mpy.VideoFileClip("test.mp4")
+input = mpy.VideoFileClip("flower.mp4")
 
-
+#resize width to input maintaining aspect ratio
 def resize(img, basewidth = 500):
     wpercent = (basewidth/float(img.size[0]))
     hsize = int((float(img.size[1])*float(wpercent)))
     return img.resize((basewidth,hsize))
 
+#runs frame at time t through edge detection filter
+## TODO: change edge detection to run off of numpy instead of Pillow
 def edgeFrame(t):
     return np.array(edge.edgeDetection(resize(Image.fromarray(input.get_frame(t)), 600),1,2))
 
@@ -22,9 +24,12 @@ clip = mpy.VideoClip(edgeFrame, duration=input.duration)
 clip.write_gif("test.gif", fps = 1)
 """
 
-## TODO: while this does multiprocess
+#multiprocess edge detection processing per frame and stich them back together into a gif
 if __name__ == '__main__':
+    time = 20
+    fps = 2
+
     p = Pool(5)
-    imgs = [p.apply(edgeFrame, args=(t,)) for t in np.linspace(0,5,10)]
-    clip = mpy.ImageSequenceClip(imgs, 2)
+    imgs = [p.apply(edgeFrame, args=(t,)) for t in np.linspace(0,time,time*fps)]
+    clip = mpy.ImageSequenceClip(imgs, fps)
     clip.write_gif("test.gif")
