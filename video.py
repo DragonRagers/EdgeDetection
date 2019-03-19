b374f517b1cb4bf7d629865c22848b8ca013a2c0
DragonRagers <dragonragers@gmail.com>
2019-03-18 23:30:33 -0400

    implemented scipy sobel instead of custom algorithm

--- video.py
+++ video.py
@@ -4,8 +4,9 @@ from PIL import Image
 import moviepy.editor as mpy
 from multiprocessing import Pool
 from tqdm import tqdm
+from scipy import ndimage
 
-input = mpy.VideoFileClip("videos\\test.mp4")
+input = mpy.VideoFileClip("videos\\flower.mp4")
 
 #resize width to input maintaining aspect ratio
 def resize(img, basewidth = 500):
@@ -13,11 +14,13 @@ def resize(img, basewidth = 500):
     hsize = int((float(img.size[1])*float(wpercent)))
     return img.resize((basewidth,hsize))
 
+"""
 #runs frame at time t through edge detection filter
 ## TODO: change edge detection to run off of numpy instead of Pillow
 def edgeFrame(t):
-    return np.array(edge.edgeDetection(resize(Image.fromarray(input.get_frame(t)), 1000),1,1))
-
+    return np.array(edge.edgeDetection(resize(Image.fromarray(input.get_frame(t)), 1080),1,1))
+    #return np.array(edge.edgeDetection(Image.fromarray(input.get_frame(t)),1,1))
+"""
 
 #single thread approach
 """
@@ -25,9 +28,21 @@ clip = mpy.VideoClip(edgeFrame, duration=input.duration)
 clip.write_gif("test.gif", fps = 1)
 """
 
+def edgeFrame(t):
+    #take frame a time = t and convert to gray scale
+    img = input.get_frame(t).dot([.07, .72, .21])
+
+    #sobel filter
+    dx = ndimage.sobel(img, 0)  # horizontal derivative
+    dy = ndimage.sobel(img, 1)  # vertical derivative
+    mag = np.hypot(dx, dy).astype(np.uint8)
+    #I don't know why i need to do this
+    return np.array(Image.fromarray(mag).convert("RGB"))
+
+
 #multiprocess edge detection processing per frame and stich them back together into a gif
 if __name__ == '__main__':
-    time = 5
+    time = round(input.duration)
     fps = 24
 
     print("Applying Edge Detection Algorithm to Frames:")
